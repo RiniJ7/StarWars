3715a317b332704eb0f60a91dff1e4279b473133
rinij <rinimani7@gmail.com>
2023-12-23 23:03:57 -0700

    Configured People API

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
-import NavBar from './NavBar'
-import Container from './Container'
-
-//the below command returns a promise
-fetch("https://swapi.dev/api/people/?page=9")
-
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(error => console.error("Error:", error));
+import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+import Container from "./Container";
 
 const HomePage = () => {
-  
+  //initialize an empty array to accept data from the api
+  const [people, setPeople] = useState([]);
 
+  const getPeople = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/people/");
+      if (!response.ok) {
+        console.log(`error ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data.results);
+      setPeople(data.results);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
+  useEffect(() => {
+    getPeople();
+  }, []);
+
+  //   //the below fetch command returns a promise
+  //   fetch("https://swapi.dev/api/people/")
+  //     //if the promise returns a value, it performs the .then methods
+  //     //here the data is converted to json (readable format)
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     //here the data hence converted is printed in the console
+  //     .then((data) => console.log(data))
+  //     //console the error if the promise is not kept/promise does not return a value
+  //     .then((data)=> data.map)
+  //     .catch((error) => console.error("Error:", error));
 
   return (
     <>
-    <NavBar/>
-    
-    <Container/>
+      <NavBar />
+     
+      <Container people={people} />
     </>
-   
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+    <div>Super Hero List</div>
+    <ul>
+  <li><a href="default.asp">People</a></li>
+  <li><a href="news.asp">Films</a></li>
+  <li><a href="contact.asp">Planets</a></li>
+  <li><a href="about.asp">Species</a></li>
+  <li><a href="contact.asp">Starships</a></li>
+  <li><a href="about.asp">Vehicles</a></li>
+</ul>
+</>
   )
 }
 
