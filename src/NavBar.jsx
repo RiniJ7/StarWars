85647f95039e7079c91272ac09408e336773b153
rinij <rinimani7@gmail.com>
2023-12-24 18:00:00 -0700

    nav bar added

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import "./NavStyles.css"
+import "./assets/StarWarsIcon.png"
 
 const NavBar = () => {
   return (
-    <>
+<nav className="nav">
+ <img className="site-title" src="./assets/StarWarsIcon.png" alt="star wars logo" />
  
+  <ul>
+  <li className="active"><a href="/" >People</a></li>
+  <li><a href="/" >Films</a></li>
+  <li><a href="/" >Planets</a></li>
+  <li><a href="/" >Species</a></li>
+  <li><a href="/" >Starships</a></li>
+  <li><a href="/" >Vehicles</a></li>
+  </ul>
+</nav>
 
-<div class="w3-bar w3-red">
- <div class="w3-bar-item w3-button">Super Hero List</div>
-  <a href="/" class="w3-bar-item w3-button">Home-People</a>
-  <a href="/" class="w3-bar-item w3-button">Films</a>
-  <a href="/" class="w3-bar-item w3-button">Planets</a>
-  <a href="/" class="w3-bar-item w3-button">Species</a>
-  <a href="/" class="w3-bar-item w3-button">Starships</a>
-  <a href="/" class="w3-bar-item w3-button">Vehicles</a>
-</div>
-{/* <div className='header-box'>
-            <div className='left-buttons'>
-                <Link to="/"><button className='all-cards-button' >All Cards</button></Link>
-            </div>
+// {/* <div className='header-box'>
+//             <div className='left-buttons'>
+//                 <Link to="/"><button className='all-cards-button' >All Cards</button></Link>
+//             </div>
             
         
-        </div> */}
-</>
+//         </div> */}
   )
 }
 
