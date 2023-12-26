3c43eb1623e142db3631a290aa5a46407ed705f7
rinij <rinimani7@gmail.com>
2023-12-25 18:04:53 -0700

    changes prop destructuring

--- src/HomeContainer.jsx
+++ src/HomeContainer.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 const HomeContainer = (props) => {
-    // destructuring people property from the props object
-    const people = props.people;
+  // destructuring people property from the props object
+  const peopleDetails = props.people;
 
-    //render the component
+  //render the component
   return (
-    <div>
-        {people.map((p) => {console.log(p);
-        return(
-            //return jsx element for each person
+    <>
+      <div>
+        {peopleDetails.length > 0 ? (
+          peopleDetails.map((p) => (
             <div className="box" key={p.url}>
-                <h2>Name: {p.name}</h2>
-                <h2>Weight of Super Hero (Kg): {p.mass}</h2>
-                <h2>Skin Color: {p.skin_color}</h2>
+              <h2>Name: {p.name}</h2>
+              <h2>Weight of Super Hero (Kg): {p.mass}</h2>
+              <h2>Skin Color: {p.skin_color}</h2>
+            </div>
+          ))
+        ) : (
+          <p> Loading People...</p>
+        )}
+      </div>
+    </>
+  );
+};
 
-                </div>
-        )
-        })}
-    </div>
-  )
-}
-
-export default HomeContainer
+export default HomeContainer;
