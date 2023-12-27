8bb05d16fa6cb8837bdc1e741d7cb7e66bd41f36
rinij <rinimani7@gmail.com>
2023-12-26 20:16:32 -0700

    half written code to fetch all people info

--- src/AllPeople.jsx
+++ src/AllPeople.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState} from "react";
 
 const AllPeople = () => {
-  return (
-    <div>AllPeople</div>
-  )
-}
 
-export default AllPeople
+  const [allPeople, setAllPeople] = useState([]);
+  const [page, setPage] = (1);
+
+  const getAllPeople = async () => {
+    try {
+      const response = await fetch(`https://swapi.dev/api/people/?page=${page}`);
+if(!Response.ok) {
+  console.log(`error ${response.status}`)
+}
+ const data = await response.json();
+      console.log(data);
+      setAllPeople(data)
+return response.json();
+    } 
+    catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(()=> {
+    getAllPeople();
+  }, []);
+
+  return (
+    <>
+      <div>AllPeople</div>
+     
+    </>
+  );
+};
+
+export default AllPeople;
